670c38b3a3a51d28a4617525d452364ace07b7c4
Vachan Kudmule <vachan@kdc.in>
2025-08-30 20:20:05 +0530

    Version Tag bumo
    changed from 1.5.0 to 1.6.0

--- camptix-kdcpay.php
+++ camptix-kdcpay.php
@@ -5,7 +5,7 @@
  * Description: KDCpay Payment Gateway for CampTix
  * Author: _KDC-Labs
  * Author URI: http://www.kdclabs.com/
- * Version: 1.5.0
+ * Version: 1.6.0
  * License: GPLv2 or later
  * Text Domain: camptix-kdcpay
  * Domain Path: /languages
